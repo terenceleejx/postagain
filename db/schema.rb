a0c1ae7c18ada2aeb9370180c9995ebcb2abc5b2
Terence Lee <terence.lee.jx@gmail.com>
2014-04-27 19:53:10 +0800

    basic table added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140426042337) do
+ActiveRecord::Schema.define(version: 20140427114432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20140426042337) do
     t.string  "due_time"
     t.text    "statistics"
     t.string  "profile_service"
+    t.integer "resent",          default: 0
   end
 
   create_table "users", force: true do |t|
