3534cc5b1fb66d18e0ff6f1c2f2a8fab310a262c
Terence Lee <terence.lee.jx@gmail.com>
2014-05-11 05:45:54 +0800

    added 0 filter. removed caching in browser

--- app/jobs/buffer_posts_update.rb
+++ app/jobs/buffer_posts_update.rb
@@ -14,31 +14,54 @@ class BufferPostsUpdate < Struct.new(:current_user_id)
 	    updates = client.updates_by_profile_id(profile_id, options = {count: 100, :status => "sent"})
 	    # first key in updates hash reveals the total number of updates
 	    updates["updates"].each do |update|
-	    	if !Post.exists?(:buffer_post_id => update["id"]) == true
-	    		Post.create({
-	    			text: update["text_formatted"],
-	    			user_id: User.find_by(uid: current_user_id).id,
-	    			profile_id: update["profile_id"],
-	    			buffer_post_id: update["id"],
-	    			buffer_user_id: update["user_id"],
-	    			created_at: update["created_at"],
-	    			day: update["day"],
-	    			due_at: update["due_at"],
-	    			due_time: update["due_time"],
-	    			sent_at: update["sent_at"],
-	    			media: update["media"],
-	    			statistics: update["statistics"],
-	    			profile_service: update["profile_service"]
-	    		})
-	    		new_post += 1
-	    	else
-	    		existing_post = Post.where(buffer_post_id: update["id"])
-	    		# Update statistics for existing post only if it was changed and if it is less than a week old.
-	    		if Time.at(update["sent_at"]) >= (Time.now - 604800).to_date
-	    		  Post.update(existing_post.select("id"), statistics: update["statistics"])
-	    		  updated_post += 1
+	  		added_key = 0
+	 		  exclude = false
+	    	if update["profile_service"] == "linkedin" || update["profile_service"] == "twitter"
+	    		update["statistics"].each do |key, value|
+	    		  if value == 0
+	    			  added_key += 1
+	    			end
+	    		end
+	    		if added_key == 4
+	    		  exclude = true
+	    		end
+	    	elsif update["profile_service"] == "facebook"
+	    		update["statistics"].each do |key, value|
+	    		  if value == 0
+	    			  added_key += 1
+	    			end
+	    		end
+	    		if added_key == 2
+	    		  exclude = true
 	    		end
 	    	end
+	    	if exclude == false
+		    	if !Post.exists?(:buffer_post_id => update["id"]) == true
+		    		Post.create({
+		    			text: update["text_formatted"],
+		    			user_id: User.find_by(uid: current_user_id).id,
+		    			profile_id: update["profile_id"],
+		    			buffer_post_id: update["id"],
+		    			buffer_user_id: update["user_id"],
+		    			created_at: update["created_at"],
+		    			day: update["day"],
+		    			due_at: update["due_at"],
+		    			due_time: update["due_time"],
+		    			sent_at: update["sent_at"],
+		    			media: update["media"],
+		    			statistics: update["statistics"],
+		    			profile_service: update["profile_service"]
+		    		})
+		    		new_post += 1
+		    	else
+		    		existing_post = Post.where(buffer_post_id: update["id"])
+		    		# Update statistics for existing post only if it was changed and if it is less than a week old.
+		    		if Time.at(update["sent_at"]) >= (Time.now - 604800).to_date
+		    		  Post.update(existing_post.select("id"), statistics: update["statistics"])
+		    		  updated_post += 1
+		    		end
+		    	end
+		    end
 	    end
 	  end
     puts "#{new_post} posts saved."
