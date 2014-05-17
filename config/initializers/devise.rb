675efa17a9bfbee5f2642aa322b7df7bc9c3e95c
Terence Lee <terence.lee.jx@gmail.com>
2014-05-17 12:54:27 +0800

    removed ssl in devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -228,8 +228,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :buffer, Figaro.env.buffer_client_id, Figaro.env.buffer_secret, 
-    {:scope => 'email, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+  config.omniauth :buffer, Figaro.env.buffer_client_id, Figaro.env.buffer_secret
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
